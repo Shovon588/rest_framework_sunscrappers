ce2c5c968ea1d34f28767c6cb0b23e1d8655854d
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-04 01:06:49 +0600

    Connecting auth token url

--- rest_framework_practice/rest_framework_practice/urls.py
+++ rest_framework_practice/rest_framework_practice/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . api import router
+import djoser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('api/auth/', include('djoser.urls.authtoken'))
 ]
