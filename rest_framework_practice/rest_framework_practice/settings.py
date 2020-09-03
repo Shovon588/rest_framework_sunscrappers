475e97b77d931176884d992aa9d3c4d63ba7995b
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-04 01:06:35 +0600

    Configuration for djoser and rest_framework.authtoken

--- rest_framework_practice/rest_framework_practice/settings.py
+++ rest_framework_practice/rest_framework_practice/settings.py
@@ -39,8 +39,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rental',
+    'djoser',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAuthenticated'
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
