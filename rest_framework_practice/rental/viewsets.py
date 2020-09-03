b82033e024f50239a02dd2a83327f2985ba813e9
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-04 01:05:57 +0600

    Permission classes added to each viewset

--- rest_framework_practice/rental/viewsets.py
+++ rest_framework_practice/rental/viewsets.py
@@ -2,18 +2,22 @@ from . models import Friend, Belonging, Borrowed
 from . serializers import FriendSerializer, BelongingSerializer, BorrowedSerializer
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
 
 class FriendViewset(viewsets.ModelViewSet):
     queryset = Friend.objects.all()
     serializer_class = FriendSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BelongingViewset(viewsets.ModelViewSet):
     queryset = Belonging.objects.all()
     serializer_class = BelongingSerializer
+    permission_classes = [AllowAny]
 
 
 class BorrowedViewSet(viewsets.ModelViewSet):
     queryset = Borrowed.objects.all()
     serializer_class = BorrowedSerializer
+    permission_classes = [IsAdminUser]
