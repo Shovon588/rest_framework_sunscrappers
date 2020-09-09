63c4657318a367abb550008de63e0b4f27c3bc71
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-09 20:40:49 +0600

    Adding the new models (ImageUpload)

--- rest_framework_practice/rental/admin.py
+++ rest_framework_practice/rental/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from . models import Friend, Belonging, Borrowed
+from . models import Friend, Belonging, Borrowed, ImageUpload
 
 # Register your models here.
 
 admin.site.register(Friend)
 admin.site.register(Belonging)
 admin.site.register(Borrowed)
-
+admin.site.register(ImageUpload)
